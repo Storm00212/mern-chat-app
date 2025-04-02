be47dd73ae033d50212d1a4446e0f81e3427fd3d
Storm00212 <166727580+Storm00212@users.noreply.github.com>
2025-04-02 10:54:37 +0300

    Add Conversations.jsx

--- frontend/vite-project/src/components/Sidebar/Conversations.jsx
+++ frontend/vite-project/src/components/Sidebar/Conversations.jsx
@@ -1,37 +1,21 @@
 import React from 'react'
 import useGetConversations from '../../hooks/useGetConversations';
 import Conversation from './Conversation';
-import { getRandomEmoji } from '../../utils/emojis';
-<script src="http://localhost:8097"></script>
 
 const Conversations = () => {
   const {loading, conversations}=useGetConversations();
   // console.log("CONVERSATIONS:",conversations);
   return (
-    <div className='py-2 flex flex-col overflow-y-autoauto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200'>
+    <div className='py-2 flex flex-col overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200'>
       {loading && <span className='loading loading-spinner'></span>}
       
 
+      {!loading && conversations.map((conversation) => (
+        <Conversation key={conversation.id} {...conversation} />
+      ))}
     </div>
   )
 }
 
 export default Conversations;
 
-/**
- * starter code for this file.
-const Conversations = () => {
-    return (
-      <div className='py-2 flex flex-col overflow-auto'>
-          <Conversation />
-          <Conversation />
-          <Conversation />
-          <Conversation />
-          <Conversation />
-          <Conversation />
-      </div>
-    )
-  }
-  
-  export default Conversations;
-    */

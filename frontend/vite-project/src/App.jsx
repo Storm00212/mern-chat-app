54f6e13fc89f44b6046f2c8878e1de1457e27ff2
Storm00212 <166727580+Storm00212@users.noreply.github.com>
2025-04-17 22:41:13 +0300

    Deployment

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import Login from './pages/home/login/login';
-import SignUp from './pages/home/signup/signup';
+import Login from './pages/home/login/Login'; // Ensure correct case
+import SignUp from './pages/home/signup/SignUp'; // Ensure correct case
 import Home from './pages/home/home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
